14338eefd9ab05d1403e6205cb62d2e0c3caec7e
松尾清美 <>
2024-10-16 19:03:15 +0900

    “シードを行いusersテーブルとsubjectsテーブルに初期データを登録する”

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+//use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +15,17 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        //usersテーブルにユーザーを追加する
+        DB::table('users')->insert([
+            ['over_name' => '松尾',
+            'under_name' => '清美',
+            'over_name_kana' =>'マツオ',
+            'under_name_kana' => 'キヨミ',
+            'mail_address' => 'matsuokiyomi@gmail.com',
+            'sex' => '2',
+            'birth_day' => '1998-3-19',
+            'role' => '1',
+            'password' => bcrypt('12345678'),]
+            ]);
     }
 }
