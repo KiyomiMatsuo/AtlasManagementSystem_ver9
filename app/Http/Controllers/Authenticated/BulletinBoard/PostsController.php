7827db10e5de71a410a074a54ca0a022ba79f240
松尾清美 <>
2024-10-29 19:15:49 +0900

    エラーメッセージの最初に ※ を入れました

--- app/Http/Controllers/Authenticated/BulletinBoard/PostsController.php
+++ app/Http/Controllers/Authenticated/BulletinBoard/PostsController.php
@@ -62,12 +62,12 @@ class PostsController extends Controller
             'post_body' => ['required', 'string' , 'max:5000'],
             'post_title' => ['required', 'string' , 'max:100'],
         ],[
-            'post_body.required' => '投稿内容は必須です',
-            'post_body.string' => '投稿は文字列で入力してください',
-            'post_body.max' => '投稿は5000文字以内で入力してください',
-            'post_title.required' => 'タイトルは必須です',
-            'post_title.string' => 'タイトルは文字列で入力してください',
-            'post_title.max' => 'タイトルは100文字以内で入力してください',
+            'post_body.required' => '※投稿内容は必須です',
+            'post_body.string' => '※投稿は文字列で入力してください',
+            'post_body.max' => '※投稿は5000文字以内で入力してください',
+            'post_title.required' => '※タイトルは必須です',
+            'post_title.string' => '※タイトルは文字列で入力してください',
+            'post_title.max' => '※タイトルは100文字以内で入力してください',
         ]);
 
         Post::where('id', $request->post_id)->update([
