df490d92ba24d5e123c9c5e865ab522d51f5f6bb
松尾清美 <>
2025-01-14 20:20:30 +0900

    予約している人数を表示

--- app/Calendars/Admin/CalendarWeekDay.php
+++ app/Calendars/Admin/CalendarWeekDay.php
@@ -31,13 +31,16 @@ class CalendarWeekDay{
 
     $html[] = '<div class="text-left">';
     if($one_part){
-      $html[] = '<p class="day_part m-0 pt-1">1部</p>';
+      $one_part_count = $one_part->users->count();
+      $html[] = '<p class="day_part m-0 pt-1">1部 '.$one_part_count.'</p>';
     }
     if($two_part){
-      $html[] = '<p class="day_part m-0 pt-1">2部</p>';
+      $two_part_count = $two_part->users->count();
+      $html[] = '<p class="day_part m-0 pt-1">2部 '.$two_part_count.'</p>';
     }
     if($three_part){
-      $html[] = '<p class="day_part m-0 pt-1">3部</p>';
+      $three_part_count = $three_part->users->count();
+      $html[] = '<p class="day_part m-0 pt-1">3部 '.$three_part_count.'</p>';
     }
     $html[] = '</div>';
 
